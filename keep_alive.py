4f17619624dffab9f43d3a4aee754d0d0519fa5e
sahilsinghexe <106299379+sahilsinghexe@users.noreply.github.com>
2024-08-12 21:23:29 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
 from threading import Thread
+from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "Alive"
+    return "SpikeSpiegel"
 
 def run():
-    app.run(host='0.0.0.0',port=8080)
+    app.run( http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
+           )
 
 def keep_alive():
     t = Thread(target=run)
-    t.start()    
+    t.start() 
